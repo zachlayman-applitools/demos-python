bcdf8a1619939f00e33cceafdaac3142acd5f98f
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-06-03 11:15:06 -0700

    initial commit

---
+++ demo.py
@@ -0,0 +1,43 @@
+import os
+
+from selenium import webdriver
+from applitools.eyes import Eyes
+from applitools.common import BatchInfo
+
+class HelloWorld:
+
+    eyes = Eyes()
+    
+    # Initialize the eyes SDK and set your private API key.
+    eyes.api_key = os.environ['APPLITOOLS_API_KEY']
+
+    try:
+        
+        # Open a Chrome browser.
+        driver = webdriver.Chrome()
+        
+        # Start the test and set the browser's viewport size to 800x600.
+        eyes.open(driver=driver, app_name='pythonAPP', test_name='pythonTEST',
+                  viewport_size={'width': 1000, 'height': 800})
+
+        b = BatchInfo(id="pythonID", name="pythonNAME")
+        b.id = "pythonID"
+
+        eyes.batch = b
+
+        # Navigate the browser to the "hello world!" web-site.
+        driver.get('https://applitools.com/helloworld?diff1')
+
+        # Visual checkpoint #1.
+        eyes.check_window('Hello!')
+
+        # End the test.
+        eyes.close()
+
+    finally:
+
+        # Close the browser.
+        driver.quit()
+
+        # If the test was aborted before eyes.close was called, ends the test as aborted.
+        eyes.abort_if_not_closed()
